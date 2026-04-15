990f2939482fc4522fc52ffe311b5ca811b01046
Little-Nightmares <155751193+Little-Nightmares@users.noreply.github.com>
2026-04-15 19:57:54 +0800

    项目构建与预编译
    之前只是框架，并且CMake.txt也有一点小问题，现在已经构建好了，可以直接用QT Creator 识别CMake.txt进行构建了

---
+++ client/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/ChatRoomClient.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/PROJECT/ChatRoom/client/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/ChatRoomClient_autogen/3YJK5W5UP7/qrc_qml.cpp" "CMakeFiles/ChatRoomClient.dir/ChatRoomClient_autogen/3YJK5W5UP7/qrc_qml.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/ChatRoomClient_autogen/3YJK5W5UP7/qrc_qml.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/ChatRoomClient_autogen/mocs_compilation.cpp" "CMakeFiles/ChatRoomClient.dir/ChatRoomClient_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/ChatRoomClient_autogen/mocs_compilation.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/core/AppManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/core/AppManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/core/AppManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/core/NavigationController.cpp" "CMakeFiles/ChatRoomClient.dir/src/core/NavigationController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/core/NavigationController.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/core/SessionManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/core/SessionManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/core/SessionManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/core/SettingsManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/core/SettingsManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/core/SettingsManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/crypto/AESEncryptor.cpp" "CMakeFiles/ChatRoomClient.dir/src/crypto/AESEncryptor.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/crypto/AESEncryptor.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/crypto/HashHelper.cpp" "CMakeFiles/ChatRoomClient.dir/src/crypto/HashHelper.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/crypto/HashHelper.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/crypto/KeyManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/crypto/KeyManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/crypto/KeyManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/crypto/RSAEncryptor.cpp" "CMakeFiles/ChatRoomClient.dir/src/crypto/RSAEncryptor.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/crypto/RSAEncryptor.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/database/ConversationDAO.cpp" "CMakeFiles/ChatRoomClient.dir/src/database/ConversationDAO.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/database/ConversationDAO.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/database/DatabaseManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/database/DatabaseManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/database/DatabaseManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/database/MessageDAO.cpp" "CMakeFiles/ChatRoomClient.dir/src/database/MessageDAO.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/database/MessageDAO.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/database/UserDAO.cpp" "CMakeFiles/ChatRoomClient.dir/src/database/UserDAO.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/database/UserDAO.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/main.cpp" "CMakeFiles/ChatRoomClient.dir/src/main.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/main.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/models/ConversationModel.cpp" "CMakeFiles/ChatRoomClient.dir/src/models/ConversationModel.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/models/ConversationModel.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/models/FriendRequestModel.cpp" "CMakeFiles/ChatRoomClient.dir/src/models/FriendRequestModel.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/models/FriendRequestModel.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/models/MessageModel.cpp" "CMakeFiles/ChatRoomClient.dir/src/models/MessageModel.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/models/MessageModel.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/models/UserModel.cpp" "CMakeFiles/ChatRoomClient.dir/src/models/UserModel.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/models/UserModel.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/network/HeartbeatManager.cpp" "CMakeFiles/ChatRoomClient.dir/src/network/HeartbeatManager.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/network/HeartbeatManager.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/network/MessageHandler.cpp" "CMakeFiles/ChatRoomClient.dir/src/network/MessageHandler.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/network/MessageHandler.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/network/PacketBuilder.cpp" "CMakeFiles/ChatRoomClient.dir/src/network/PacketBuilder.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/network/PacketBuilder.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/network/TcpClient.cpp" "CMakeFiles/ChatRoomClient.dir/src/network/TcpClient.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/network/TcpClient.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/AppCore.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/AppCore.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/AppCore.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/ChatController.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/ChatController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/ChatController.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/FileTransferController.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/FileTransferController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/FileTransferController.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/FriendController.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/FriendController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/FriendController.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/NotificationController.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/NotificationController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/NotificationController.cpp.obj.d"
+  "D:/PROJECT/ChatRoom/client/src/qmlbridge/UserController.cpp" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/UserController.cpp.obj" "gcc" "CMakeFiles/ChatRoomClient.dir/src/qmlbridge/UserController.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
